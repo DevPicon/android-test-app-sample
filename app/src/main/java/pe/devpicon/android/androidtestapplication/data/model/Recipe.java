fadf9d21beceead76b47d9a5c6df5858a34f9f37
apiconz <picondev@gmail.com>
2017-09-04 16:27:14 -0500

    First test completed

--- app/src/main/java/pe/devpicon/android/androidtestapplication/data/model/Recipe.java
+++ app/src/main/java/pe/devpicon/android/androidtestapplication/data/model/Recipe.java
@@ -1,12 +1,17 @@
 package pe.devpicon.android.androidtestapplication.data.model;
 
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 
 /**
  * Created by armando on 9/4/17.
  */
 
 public class Recipe {
+    private static final String ID_PREFIX = "id=";
+    private static final String TITLE_PREFIX = "title=";
     public final String id;
     public final String title;
     public final String description;
@@ -21,6 +26,29 @@ public class Recipe {
         String id = null;
         String title = null;
         StringBuilder descriptionBuilder = new StringBuilder();
+
+        BufferedReader reader = new BufferedReader(new InputStreamReader(stream));
+
+        try {
+            for (String line = reader.readLine(); line != null; line = reader.readLine()){
+                if(line.startsWith(ID_PREFIX)){
+                    id = line.substring(ID_PREFIX.length());
+                    continue;
+                }
+
+                if(line.startsWith(TITLE_PREFIX)){
+                    title = line.substring(TITLE_PREFIX.length());
+                    continue;
+                }
+
+                if(descriptionBuilder.length() > 0){
+                    descriptionBuilder.append("\n");
+                }
+                descriptionBuilder.append(line);
+            }
+        } catch (IOException e) {
+            return null;
+        }
 
         return new Recipe(id, title, descriptionBuilder.toString());
     }
--- app/src/test/java/pe/devpicon/android/androidtestapplication/data/model/RecipeTest.java
+++ app/src/test/java/pe/devpicon/android/androidtestapplication/data/model/RecipeTest.java
@@ -4,6 +4,7 @@ import org.junit.Test;
 
 import java.io.InputStream;
 
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 
 /**
@@ -12,8 +13,11 @@ import static org.junit.Assert.assertNotNull;
 public class RecipeTest {
     @Test
     public void water() {
-        InputStream stream = RecipeTest.class.getResourceAsStream("/recipe/water.txt");
+        InputStream stream = RecipeTest.class.getResourceAsStream("/recipes/water.txt");
         Recipe recipe = Recipe.readFromStream(stream);
         assertNotNull(recipe);
+        assertEquals("water", recipe.id);
+        assertEquals("Water", recipe.title);
+        assertEquals("Put glass under tap. Open tap. Close tap. Drink.", recipe.description);
     }
 }
