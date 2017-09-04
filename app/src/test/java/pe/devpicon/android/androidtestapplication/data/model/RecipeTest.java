6651eab9ff00f6e4625c84b66047b60c009f389f
apiconz <picondev@gmail.com>
2017-09-04 15:14:04 -0500

    Implementing Green phase

--- app/src/test/java/pe/devpicon/android/androidtestapplication/data/model/RecipeTest.java
+++ app/src/test/java/pe/devpicon/android/androidtestapplication/data/model/RecipeTest.java
@@ -1,8 +1,19 @@
 package pe.devpicon.android.androidtestapplication.data.model;
 
+import org.junit.Test;
+
+import java.io.InputStream;
+
+import static org.junit.Assert.assertNotNull;
+
 /**
  * Created by armando on 9/4/17.
  */
 public class RecipeTest {
-
+    @Test
+    public void water() {
+        InputStream stream = RecipeTest.class.getResourceAsStream("/recipe/water.txt");
+        Recipe recipe = Recipe.readFromStream(stream);
+        assertNotNull(recipe);
+    }
 }
