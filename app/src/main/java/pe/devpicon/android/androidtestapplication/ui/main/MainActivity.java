98bf279d2d7a18735dee84eae53e70165109affb
apiconz <picondev@gmail.com>
2017-09-04 14:59:36 -0500

    Add base project

--- app/src/main/java/pe/devpicon/android/androidtestapplication/ui/main/MainActivity.java
+++ app/src/main/java/pe/devpicon/android/androidtestapplication/ui/main/MainActivity.java
@@ -1,7 +1,9 @@
-package pe.devpicon.android.androidtestapplication;
+package pe.devpicon.android.androidtestapplication.ui.main;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import pe.devpicon.android.androidtestapplication.R;
 
 public class MainActivity extends AppCompatActivity {
 
